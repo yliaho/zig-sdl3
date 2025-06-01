4111827921b69923df95a7385ec93ad3f829c4c6
Joonas Yliaho <joonas.yliaho@gmail.com>
2025-06-01 15:12:35 +0300

    remove gamepad wip support

--- src/events.zig
+++ src/events.zig
@@ -287,7 +287,7 @@ pub const Type = enum(c.SDL_EventType) {
     // JoystickBatteryUpdated,
     // JoystickUpdateComplete,
     // GamepadAxisMotion,
-    gamepad_button_down = c.SDL_EVENT_GAMEPAD_BUTTON_DOWN,
+    // GamepadButtonDown
     // GamepadButtonUp,
     // GamepadAdded,
     // GamepadRemoved,
@@ -479,17 +479,6 @@ pub const MouseButton = struct {
     y: f32,
 };
 
-pub const GamePadButton = struct {
-    /// Common event information.
-    common: Common,
-    /// The gamepad instance id.
-    which: ?keyboard.ID,
-    /// The gamepad button.
-    button: u8,
-    /// True if the button is pressed.
-    down: bool,
-};
-
 /// The "quit requested" event.
 pub const Quit = struct {
     /// Common event information.
@@ -522,7 +511,6 @@ pub const Event = union(Type) {
     mouse_button_down: MouseButton,
     /// A mouse released event.
     mouse_button_up: MouseButton,
-    gamepad_button_down: GamePadButton,
     /// A user event.
     user: User,
     /// An unknown event.
@@ -618,16 +606,6 @@ pub const Event = union(Type) {
                     .clicks = event.button.clicks,
                     .x = event.button.x,
                     .y = event.button.y,
-                },
-            },
-            c.SDL_EVENT_GAMEPAD_BUTTON_DOWN => .{
-                .gamepad_button_down = .{
-                    .common = Common.fromSdl(&event),
-                    .which = if (event.gbutton.which == 0) null else .{
-                        .value = event.gbutton.which,
-                    },
-                    .button = event.gbutton.button,
-                    .down = event.gbutton.down,
                 },
             },
             c.SDL_EVENT_ENUM_PADDING => .{
